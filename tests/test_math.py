4a844eb851c0de679395dae2be77c588904fb38c
anonUnderground <ahmed.alkarboly@gmail.com>
2024-04-19 15:37:43 -0500

    Initial commit

--- tests/test_math.py
+++ tests/test_math.py
@@ -13,3 +13,19 @@ def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1 / 0
     assert 'division by zero' in str(e.value)
+
+#Test multiplication
+
+products = [
+    (2,3,6),
+    (3,-4,-12),
+    (-5,-5,25),
+    (1,99,99),
+    (0,99,0),
+    (2.5,6.7,17.75)
+]
+
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_product(a,b,product):
+    assert a*b == product
